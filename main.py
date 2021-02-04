163e92f6bc64b9dbf87f814947c741779167c8bf
kressin <luiz.kressin@gmail.com>
2021-02-04 12:09:01 -0300

    loops hello world to infinite

--- main.py
+++ main.py
@@ -11,7 +11,8 @@ class Output(asyncio.Protocol):
     def data_received(self, data):
         print('data received', repr(data))
         if b'\n' in data:
-            self.transport.close()
+            self.transport.write(b'Hello, World!\n')
+#            self.transport.close()
 
     def connection_lost(self, exc):
         print('port closed')
@@ -25,9 +26,25 @@ class Output(asyncio.Protocol):
         print(self.transport.get_write_buffer_size())
         print('resume writing')
 
-loop = asyncio.get_running_loop()
+# https://docs.python.org/3/library/asyncio-eventloop.html
+# asyncio.get_event_loop() :
+# Function: get the current event loop OR create a new event loop and set it as the current one
+# IF there is no current event loop set in the current OS thread,
+# AND set_event_loop() has not yet been called
+# THEN asyncio will create a new event loop and set it as the current one
+loop = asyncio.get_event_loop()
 
+# serial_asyncio.create_serial_connection(loop, protocol_factory, *args, **kwargs)
+#   Function: Get a connection making coroutine
+# Parameters:
+#           loop - The event handler
+#           protocol_factory - Factory function(asyncio.coroutine) for a asyncio.Protocol
+#           args, kwargs - Passed to the serial.Serial init function
 coro = serial_asyncio.create_serial_connection(loop, Output, '/dev/ttyAMA0', baudrate=2000000)
+
+# loop.run_until_complete(future)
+# Function: run until the future(an instance of Future) has completed
+#           If the argument is a coroutine object it is implicitly scheduled to run as a asyncio.Task
 loop.run_until_complete(coro)
 
 loop.run_forever()

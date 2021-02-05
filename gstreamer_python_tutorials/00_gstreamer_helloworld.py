e0cb2bb9e58e9eee1969037cd819ea4653b32e20
kressin <luiz.kressin@gmail.com>
2021-02-05 13:28:48 -0300

    gstreamer python encodes to h264

--- gstreamer_python_tutorials/00_gstreamer_helloworld.py
+++ gstreamer_python_tutorials/00_gstreamer_helloworld.py
@@ -20,7 +20,7 @@ Gst.init(sys.argv[1:])
 # build the pipeline
 # Gst.parse_launch:
 pipeline = Gst.parse_launch(
-    "v4l2src device=/dev/video0 ! video/x-raw,width=640,height=480,framerate=30/1 ! videoconvert ! jpegenc ! rtpjpegpay ! udpsink host=192.168.1.23"
+    "v4l2src device=/dev/video0 ! video/x-raw,width=640,height=480,framerate=30/1 ! videoconvert ! x264enc ! rtph264pay ! udpsink host=192.168.1.23 port=5000"
 )
 
 # start playing

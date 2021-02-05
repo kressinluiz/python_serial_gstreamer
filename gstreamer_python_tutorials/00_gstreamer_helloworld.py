69b3b8f2bb15c6dd7ac555584c450a56445b6429
kressin <luiz.kressin@gmail.com>
2021-02-05 14:06:43 -0300

    gstreamer python simplifies pipeline 3

--- gstreamer_python_tutorials/00_gstreamer_helloworld.py
+++ gstreamer_python_tutorials/00_gstreamer_helloworld.py
@@ -20,7 +20,7 @@ Gst.init(sys.argv[1:])
 # build the pipeline
 # Gst.parse_launch:
 pipeline = Gst.parse_launch(
-    "v4l2src device=/dev/video0 ! video/x-raw,width=640,height=480,framerate=30/1 ! x264enc ! h264parse ! rtph264pay ! udpsink host=192.168.1.23 port=5000"
+    "v4l2src device=/dev/video0 ! video/x-raw,width=640,height=480,framerate=30/1 ! videoconvert ! x264enc pass=qual quantizer=20 tune=zerolatency ! rtph264pay ! udpsink host=192.168.1.23 port=5000"
 )
 
 # start playing

b37498931191c328b9187a0e5d9286ee9ec21762
kressin <luiz.kressin@gmail.com>
2021-02-05 11:36:20 -0300

    gstreamer python basic concepts

--- gstreamer_python_tutorials/00_gstreamer_helloworld.py
+++ gstreamer_python_tutorials/00_gstreamer_helloworld.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+#credits : https://gstreamer.freedesktop.org/documentation/tutorials/basic/hello-world.html?gi-language=python
+
 import sys
 import gi
 
@@ -16,6 +18,7 @@ message = None
 Gst.init(sys.argv[1:])
 
 # build the pipeline
+# Gst.parse_launch:
 pipeline = Gst.parse_launch(
     "playbin uri=https://www.freedesktop.org/software/gstreamer-sdk/data/media/sintel_trailer-480p.webm"
 )
@@ -23,8 +26,9 @@ pipeline = Gst.parse_launch(
 # start playing
 pipeline.set_state(Gst.State.PLAYING)
 
-# wait until EOS or error
-bus = pipeline.get_bus()
+## wait until EOS (END OF STREAM) or error
+bus = pipeline.get_bus() #retrieves the pipeline's bus
+#blocks until EOS or error
 msg = bus.timed_pop_filtered(
     Gst.CLOCK_TIME_NONE,
     Gst.MessageType.ERROR | Gst.MessageType.EOS
